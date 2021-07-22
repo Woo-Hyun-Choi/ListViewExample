73dd96b8981a2db6d037ce5f4cdd2ccb4694ce77
최우현 <whchoi@hutek.net>
2021-07-22 19:34:42 +0900

    implement Adapter & Add listview_item.xml

--- app/src/main/java/net/hutek/listviewexample/MainActivity.java
+++ app/src/main/java/net/hutek/listviewexample/MainActivity.java
@@ -1,9 +1,15 @@
 package net.hutek.listviewexample;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -14,8 +20,11 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final static String TAG = "whchoi";
+
     TextView textView;
     ListView listView;
+    ArrayList items = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,32 +34,118 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.listView);
         textView = findViewById(R.id.textView);
 
-        // 데이터를 저장하게 되는 리스트
-        List<String> list = new ArrayList<>();
 
-        // 리스트뷰와 리스트를 연결하기 위해 사용되는 어댑터
-        // context, layout Res int source, List object
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice, list);
+        init_ArrayList(20);
+
+        ListView_Adapter mAdapter = new ListView_Adapter(this, items);
 
         // 리스트뷰의 어댑터를 지정해준다.
-        listView.setAdapter(adapter);
+        listView.setAdapter(mAdapter);
+
 
         // 리스트뷰의 아이템을 클릭시 해당 아이템의 문자열을 가져오기 위한 처리
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 // 클릭한 아이템의 문자열을 가져옴
-                String selected_item = (String) adapterView.getItemAtPosition(position);
+                ListView_Item item = (ListView_Item) adapterView.getItemAtPosition(position);
                 //텍스트뷰에 출력
-                textView.setText(selected_item + "를 선택하셨습니다");
-
+                String title = item.getTitle();
+                Log.d(TAG, title);
             }
         });
+    }
 
-        for(int i=1; i <=9999; i++) {
-            list.add(i+"번째");
+
+
+    // item 데이터 객체, 클래스 생성
+    private class ListView_Item {
+        // 아이템 당 각각의 내용과 title이 담길 변수를 선언해준다
+        private String title;
+        private int image;
+
+        // 생성자 함수
+        public ListView_Item(String title, int image){
+            this.title = title;
+            this.image = image;
         }
 
+        public String getTitle() {
+            return title;
+        }
 
+        public int getImage() {
+            return image;
+        }
+    }
+
+
+
+    // Adapter 생성 - ListView 연결
+    // ListView를 표현할 Adapter 구현, BaseAdapter 상속
+    private class ListView_Adapter extends BaseAdapter {
+
+        //보여줄 Item 몰록을 저장할 List
+        List<ListView_Item> items = null;
+        Context context;
+
+        public ListView_Adapter(Context context, List<ListView_Item> items){
+            this.items = items;
+            this.context = context;
+        }
+
+        // Adapter.getCount(), 아이템 개수 반환하는 함수
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        // Adapter.getItem(int position), 해당 위치 아이템 반환 함수
+        @Override
+        public ListView_Item getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Log.e(TAG, "getView :: position = "+position);
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view = layoutInflater.inflate(R.layout.listview_item, parent, false);
+
+            // ListView의 Item을 구성하는 뷰 연결
+            TextView number = view.findViewById(R.id.listitem_number);
+            TextView title = view.findViewById(R.id.listitem_title);
+            ImageView image = view.findViewById(R.id.listitem_image);
+
+            // ListView의 Item을 구성하는 뷰 세팅
+            ListView_Item item = items.get(position);
+            number.setText(String.valueOf(position+1));
+            title.setText(item.getTitle());
+            image.setImageResource(item.getImage());
+
+
+            return view;
+
+        }
+    }
+
+    // Item 리스트를 생성하는 함수
+    private void init_ArrayList(int count) {
+
+        // Drawable 이미지 리소스 ID 값을 가져오기 위해 Resource객체 생성
+        Resources res = getResources();
+
+        // 함수의 인자로 넘겨준 count 아이템 개수만큼 반복, 아이템 추가
+        for (int i = 0; i < count; i++) {
+            // 이미지리소스 id 값을 가져옴, res.getIdentifier("이미지 이름", "리소스 폴더 이름", 현재패키지 이름)
+            int img_ID = res.getIdentifier("listview_item" + (i % 4), "drawable", getPackageName());
+            // item 객체 생성하여 리스트에 추가
+            items.add(new ListView_Item((i + 1) + "번째 아이템", img_ID));
+        }
     }
 }
