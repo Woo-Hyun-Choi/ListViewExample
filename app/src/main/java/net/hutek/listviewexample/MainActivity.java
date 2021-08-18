e26ae7835de5cc0252970552e0f36c399e07a376
최우현 <whchoi@hutek.net>
2021-08-18 19:55:51 +0900

    listView item 수정

--- app/src/main/java/net/hutek/listviewexample/MainActivity.java
+++ app/src/main/java/net/hutek/listviewexample/MainActivity.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final static String TAG = "whchoi";
+    private final static String TAG = "ListViewExample";
 
     TextView textView;
     ListView listView;
@@ -56,40 +56,65 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    // Item 리스트를 생성하는 함수
+    private void init_ArrayList(int count) {
 
+        // Drawable 이미지 리소스 ID 값을 가져오기 위해 Resource객체 생성
+        Resources res = getResources();
+
+        // 함수의 인자로 넘겨준 count 아이템 개수만큼 반복, 아이템 추가
+        for (int i = 0; i < count; i++) {
+            // 이미지리소스 id 값을 가져옴, res.getIdentifier("이미지 이름", "리소스 폴더 이름", 현재패키지 이름)
+            int img_ID = res.getIdentifier("listview_item" + (i % 4), "drawable", getPackageName());
+            // item 객체 생성하여 리스트에 추가
+            items.add(new ListView_Item((i + 1) + "번째 아이템", img_ID, "hello", "world"));
+            Log.d(TAG, "이것은 " + img_ID);
+        }
+    }
 
     // item 데이터 객체, 클래스 생성
     private class ListView_Item {
         // 아이템 당 각각의 내용과 title이 담길 변수를 선언해준다
+        private int thumbnail;
         private String title;
-        private int image;
+        private String duration;
+        private String resolution;
 
         // 생성자 함수
-        public ListView_Item(String title, int image){
+        public ListView_Item(String title, int image, String duration, String resolution) {
+            this.thumbnail = image;
             this.title = title;
-            this.image = image;
+            this.duration = duration;
+            this.resolution = resolution;
+        }
+
+        public int getThumbnail() {
+            return thumbnail;
         }
 
         public String getTitle() {
             return title;
         }
 
-        public int getImage() {
-            return image;
+        public String getDuration() {
+            return duration;
+        }
+
+        public String getResolution() {
+            return resolution;
         }
     }
-
-
 
     // Adapter 생성 - ListView 연결
     // ListView를 표현할 Adapter 구현, BaseAdapter 상속
     private class ListView_Adapter extends BaseAdapter {
 
+
         //보여줄 Item 몰록을 저장할 List
         List<ListView_Item> items = null;
         Context context;
 
-        public ListView_Adapter(Context context, List<ListView_Item> items){
+        public ListView_Adapter(Context context, List<ListView_Item> items) {
             this.items = items;
             this.context = context;
         }
@@ -113,40 +138,25 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            Log.e(TAG, "getView :: position = "+position);
+            Log.e(TAG, "getView :: position = " + position);
             LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             View view = layoutInflater.inflate(R.layout.listview_item, parent, false);
 
             // ListView의 Item을 구성하는 뷰 연결
-//            TextView number = view.findViewById(R.id.listitem_number);
+            ImageView thumbnail = view.findViewById(R.id.listitem_thumbnail);
             TextView title = view.findViewById(R.id.listitem_title);
-            ImageView image = view.findViewById(R.id.listitem_image);
-            ImageView date = view.findViewById(R.id.listitem_date);
+            TextView duration = view.findViewById(R.id.listitem_duration);
+            TextView resolution = view.findViewById(R.id.listitem_resolution);
 
             // ListView의 Item을 구성하는 뷰 세팅
             ListView_Item item = items.get(position);
-//            number.setText(String.valueOf(position+1));
+            thumbnail.setImageResource(item.getThumbnail());
             title.setText(item.getTitle());
-            image.setImageResource(item.getImage());
-
+            duration.setText(item.getDuration());
+            resolution.setText(item.getResolution());
 
             return view;
 
-        }
-    }
-
-    // Item 리스트를 생성하는 함수
-    private void init_ArrayList(int count) {
-
-        // Drawable 이미지 리소스 ID 값을 가져오기 위해 Resource객체 생성
-        Resources res = getResources();
-
-        // 함수의 인자로 넘겨준 count 아이템 개수만큼 반복, 아이템 추가
-        for (int i = 0; i < count; i++) {
-            // 이미지리소스 id 값을 가져옴, res.getIdentifier("이미지 이름", "리소스 폴더 이름", 현재패키지 이름)
-            int img_ID = res.getIdentifier("listview_item" + (i % 4), "drawable", getPackageName());
-            // item 객체 생성하여 리스트에 추가
-            items.add(new ListView_Item((i + 1) + "번째 아이템", img_ID));
         }
     }
 }
